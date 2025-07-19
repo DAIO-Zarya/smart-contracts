0b62e7811988c6a8e60129f5d115aad2d7cf6472
Oleg Bedrin <simplavero@gmail.com>
2025-07-20 04:05:27 +0500

    Добавление библиотеки для голосований

--- src/Zarya.sol
+++ src/Zarya.sol
@@ -6,75 +6,142 @@ import {EnumerableSet} from "@openzeppelin-contracts-5.4.0-rc.1/utils/structs/En
 import {Regions} from "./libraries/Regions.sol";
 import {PartyOrgans} from "./libraries/PartyOrgans.sol";
 import {Matricies} from "./libraries/Matricies.sol";
+import {Votings} from "./libraries/Votings.sol";
 
 contract Zarya {
-    using EnumerableSet for EnumerableSet.AddressSet;
     using Regions for Regions.Region;
-    using Matricies for Matricies.PairOfMatricies;
+    using EnumerableSet for EnumerableSet.AddressSet;
+    using Votings for Votings.Voting;
 
-    struct MemberSuggestion {
-        PartyOrgans.PartyOrgan organ;
-        address member;
-    }
+    mapping(uint256 => Votings.Voting) internal _votings;
+    uint256 internal _nextVotingId;
 
-    struct CategorySuggestion {
-        PartyOrgans.PartyOrgan organ;
-        uint256 x;
-        uint256 y;
-        uint64 category;
-    }
-
-    struct DecimalsSuggestion {
-        PartyOrgans.PartyOrgan organ;
-        uint256 x;
-        uint256 y;
-        uint8 decimals;
-    }
-
-    struct ThemeSuggestion {
-        bool isCategorical;
-        uint256 x;
-        string theme;
-    }
-
-    struct StatementSuggestion {
-        bool isCategorical;
-        uint256 x;
-        uint256 y;
-        string statement;
-    }
-
-    struct CategoricalValueSuggestion {
-        PartyOrgans.PartyOrgan organ;
-        uint256 x;
-        uint256 y;
-        uint64 value;
-        address author;
-    }
-
-    struct NumericalValueSuggestion {
-        PartyOrgans.PartyOrgan organ;
-        uint256 x;
-        uint256 y;
-        uint64 value;
-        address author;
-    }
-
-    mapping(PartyOrgans.PartyOrgan => EnumerableSet.AddressSet members) internal _membersByOrgan;
-
+    PartyOrgans.MembersRegistry internal _partyMembersRegistry;
     Matricies.PairOfMatricies internal _matricies;
 
-    // suggest new member to an organ
+    modifier onlyActiveMember(PartyOrgans.PartyOrgan organ) {
+        if (!_partyMembersRegistry.membersByOrgan[organ].contains(msg.sender)) {
+            revert PartyOrgans.NotActiveMember(organ, msg.sender);
+        }
+        _;
+    }
 
-    // suggest new category to be added for categorical matrix cell
+    modifier votingExists(uint256 votingId) {
+        if (votingId >= _nextVotingId) {
+            revert Votings.VotingNotFound(votingId);
+        }
+        _;
+    }
 
-    // suggest new decimals to be added for numerical matrix cell
+    function createMembershipVoting(PartyOrgans.PartyOrgan organ, address member, uint256 duration)
+        external
+        onlyActiveMember(organ)
+        returns (uint256 votingId)
+    {
+        votingId = ++_nextVotingId;
+        _votings[votingId].createMembershipVoting(votingId, msg.sender, duration, organ, member);
+    }
 
-    // suggest new theme to be added for a cell
+    function createCategoryVoting(PartyOrgans.PartyOrgan organ, uint256 x, uint256 y, uint64 category, uint256 duration)
+        external
+        onlyActiveMember(organ)
+        returns (uint256 votingId)
+    {
+        votingId = ++_nextVotingId;
+        _votings[votingId].createCategoryVoting(votingId, msg.sender, duration, organ, x, y, category);
+    }
 
-    // suggest new statement to be added for a cell
+    function createDecimalsVoting(PartyOrgans.PartyOrgan organ, uint256 x, uint256 y, uint8 decimals, uint256 duration)
+        external
+        onlyActiveMember(organ)
+        returns (uint256 votingId)
+    {
+        votingId = ++_nextVotingId;
+        _votings[votingId].createDecimalsVoting(votingId, msg.sender, duration, organ, x, y, decimals);
+    }
 
-    // suggest new value to be added for categorical matrix cell
+    function createThemeVoting(bool isCategorical, uint256 x, string memory theme, uint256 duration)
+        external
+        returns (uint256 votingId)
+    {
+        votingId = ++_nextVotingId;
+        _votings[votingId].createThemeVoting(votingId, msg.sender, duration, isCategorical, x, theme);
+    }
 
-    // suggest new value to be added for numerical matrix cell
+    function createStatementVoting(bool isCategorical, uint256 x, uint256 y, string memory statement, uint256 duration)
+        external
+        returns (uint256 votingId)
+    {
+        votingId = ++_nextVotingId;
+        _votings[votingId].createStatementVoting(votingId, msg.sender, duration, isCategorical, x, y, statement);
+    }
+
+    function createCategoricalValueVoting(
+        PartyOrgans.PartyOrgan organ,
+        uint256 x,
+        uint256 y,
+        uint64 value,
+        address valueAuthor,
+        uint256 duration
+    ) external onlyActiveMember(organ) returns (uint256 votingId) {
+        votingId = ++_nextVotingId;
+        _votings[votingId].createCategoricalValueVoting(votingId, msg.sender, duration, organ, x, y, value, valueAuthor);
+    }
+
+    function createNumericalValueVoting(
+        PartyOrgans.PartyOrgan organ,
+        uint256 x,
+        uint256 y,
+        uint64 value,
+        address valueAuthor,
+        uint256 duration
+    ) external onlyActiveMember(organ) returns (uint256 votingId) {
+        votingId = ++_nextVotingId;
+        _votings[votingId].createNumericalValueVoting(votingId, msg.sender, duration, organ, x, y, value, valueAuthor);
+    }
+
+    // Voting participation functions
+    function castVote(uint256 votingId, bool support, PartyOrgans.PartyOrgan organ)
+        external
+        votingExists(votingId)
+        onlyActiveMember(organ)
+    {
+        _votings[votingId].castVote(support, msg.sender);
+    }
+
+    function executeVoting(uint256 votingId, uint256 minimumQuorum, uint256 minimumApprovalPercentage)
+        external
+        votingExists(votingId)
+        returns (bool success)
+    {
+        return _votings[votingId].executeVoting(
+            minimumQuorum, minimumApprovalPercentage, _matricies, _partyMembersRegistry
+        );
+    }
+
+    // View functions
+    function getVotingResults(uint256 votingId)
+        external
+        view
+        votingExists(votingId)
+        returns (Votings.VoteResults memory)
+    {
+        return _votings[votingId].getVoteResults();
+    }
+
+    function hasVoted(uint256 votingId, address member) external view votingExists(votingId) returns (bool) {
+        return _votings[votingId].hasPartyMemberVoted(member);
+    }
+
+    function isVotingActive(uint256 votingId) external view votingExists(votingId) returns (bool) {
+        return _votings[votingId].isActive();
+    }
+
+    function isVotingFinalized(uint256 votingId) external view votingExists(votingId) returns (bool) {
+        return _votings[votingId].isFinalized();
+    }
+
+    function getNextVotingId() external view returns (uint256) {
+        return _nextVotingId;
+    }
 }
--- src/libraries/PartyOrgans.sol
+++ src/libraries/PartyOrgans.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.28;
 
+import {EnumerableSet} from "@openzeppelin-contracts-5.4.0-rc.1/utils/structs/EnumerableSet.sol";
+
 import {Regions} from "./Regions.sol";
 
 library PartyOrgans {
+    using EnumerableSet for EnumerableSet.AddressSet;
     using Regions for Regions.Region;
 
     type PartyOrgan is bytes32;
@@ -19,12 +22,17 @@ library PartyOrgans {
         Congress
     }
 
+    struct MembersRegistry {
+        mapping(PartyOrgan => EnumerableSet.AddressSet members) membersByOrgan;
+    }
+
     string internal constant CONGRESS_POSTFIX = unicode"СЗД";
     string internal constant SOVIET_POSTFIX = unicode"СОВ";
     string internal constant CHAIRPERSON_POSTFIX = unicode"ПРЛ";
     string internal constant GENERAL_ASSEMBLY_POSTFIX = unicode"ОБС";
     string internal constant CONFERENCE_POSTFIX = unicode"КОН";
 
+    error NotActiveMember(PartyOrgan organ, address caller);
     error InvalidPartyOrganType(PartyOrganType organType);
 
     function from(PartyOrganType organType, Regions.Region region, uint256 number) internal pure returns (PartyOrgan) {
@@ -38,15 +46,15 @@ library PartyOrgans {
         returns (string memory)
     {
         if (organType == PartyOrganType.LocalSoviet) {
-            return string(abi.encodePacked(Regions.toString(region), ".", number, ".", SOVIET_POSTFIX));
+            return string(abi.encodePacked(region.toString(), ".", number, ".", SOVIET_POSTFIX));
         } else if (organType == PartyOrganType.LocalGeneralAssembly) {
-            return string(abi.encodePacked(Regions.toString(region), ".", number, ".", GENERAL_ASSEMBLY_POSTFIX));
+            return string(abi.encodePacked(region.toString(), ".", number, ".", GENERAL_ASSEMBLY_POSTFIX));
         } else if (organType == PartyOrganType.RegionalSoviet) {
-            return string(abi.encodePacked(Regions.toString(region), ".", SOVIET_POSTFIX));
+            return string(abi.encodePacked(region.toString(), ".", SOVIET_POSTFIX));
         } else if (organType == PartyOrganType.RegionalConference) {
-            return string(abi.encodePacked(Regions.toString(region), ".", CONFERENCE_POSTFIX));
+            return string(abi.encodePacked(region.toString(), ".", CONFERENCE_POSTFIX));
         } else if (organType == PartyOrganType.RegionalGeneralAssembly) {
-            return string(abi.encodePacked(Regions.toString(region), ".", GENERAL_ASSEMBLY_POSTFIX));
+            return string(abi.encodePacked(region.toString(), ".", GENERAL_ASSEMBLY_POSTFIX));
         } else if (organType == PartyOrganType.Chairperson) {
             return CHAIRPERSON_POSTFIX;
         } else if (organType == PartyOrganType.CentralSoviet) {
